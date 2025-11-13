f750be5a236d52ae349ebfe88ab0fe5f0ffff39c
Pedro Filipe <piedro123@hotmail.com>
2025-11-13 12:09:12 -0300

    Adicionei o beforeEach para acesso a página de login

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,31 +1,29 @@
 describe("Login", () => {
-  it("Login com dados validos deve permitir entrada no sistema", () => {
-    //Arrange
+  beforeEach(()=>{ //por estar no before each roda antes de cada it, não precisando estar dentro do it.
+   //Arrange
     cy.visit("http://localhost:4000");
 
+  })
+
+  it("Login com dados validos deve permitir entrada no sistema", () => {
     //Act
     cy.get("#username").click().type("julio.lima");
     cy.get("#senha").click().type("123456");
-    cy.get('#login-section > .btn').click()
+    cy.get("#login-section > .btn").click();
 
     //Assert
-    cy.contains('h4', 'Realizar Transferência').should('be.visible')
+    cy.contains("h4", "Realizar Transferência").should("be.visible");
   });
 
   it("Login com dados invalidos deve apresentar mensagem de error", () => {
-    //Arrange
-    cy.visit("http://localhost:4000");
-
     //Act
     cy.get("#username").click().type("julio.lima");
     cy.get("#senha").click().type("654321");
-    cy.get('#login-section > .btn').click()
+    cy.get("#login-section > .btn").click();
 
     //Assert
-    cy.get('.toast')
-      .should('have.text','Erro no login. Tente novamente.' )
-      .should('be.visible')
-      
+    cy.get(".toast")
+      .should("have.text", "Erro no login. Tente novamente.")
+      .should("be.visible");
   });
-
 });
